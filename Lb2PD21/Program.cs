b24db81fda963fe9d3bd6c6568bfeddb0daa7c20
DESKTOP-HRB3487\Mi <jaroschew@gmail.com>
2025-03-12 16:16:18 +0200

    Add Lb2

---
+++ Lb2PD21/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lb2PD21
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            
+        }
+    }
+}
---
+++ Lb2PD21/User.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lb2PD21
+{
+    internal class User
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        private string _password { get; set; }
+
+        public User(string userName, string email, string password)
+        {
+            UserName = userName;
+            Email = email;
+            _password = password;
+        }
+
+        public void SetPassword(string email, string ondPassword, string newPassword)
+        {
+            if (!Authenticate(email, ondPassword))
+                return;
+            _password = newPassword;
+        }
+
+        public bool Authenticate(string email, string inputPassword)
+        {
+            return email == Email && _password == inputPassword;
+        }
+
+        public virtual void DisplayInfo()
+        {
+            Console.WriteLine($"UserName: {UserName}");
+            Console.WriteLine($"Email: {Email}");
+        }
+    }
+}
