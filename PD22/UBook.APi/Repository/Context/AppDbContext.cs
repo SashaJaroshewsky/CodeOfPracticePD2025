e8000ba98bc498d7c39f519831e690eefbcffb14
DESKTOP-BFGS2MG\jaros <jaroschew@gmail.com>
2025-04-30 13:33:16 +0300

    Add DbContext

--- PD22/UBook.APi/Repository/Context/AppDbContext.cs
+++ PD22/UBook.APi/Repository/Context/AppDbContext.cs
@@ -8,7 +8,7 @@ namespace UBook.API.Repository.Context
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
         }
-
+       
         public DbSet<User> Users { get; set; }
     //    public DbSet<Book> Books { get; set; } = null!;
     //    public DbSet<Author> Authors { get; set; } = null!;
